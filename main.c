ce21aa28814c10a5bcc91d3330d17e346fcb8035
Bx <53805165+92EPk@users.noreply.github.com>
2024-04-02 00:40:02 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,6 @@
 #include "monty.h"
 stack_t *head = NULL;
 
-/**
- * main - entry point
- * @argc: arguments count
- * @argv: list of arguments
- * Return: always 0
- */
-
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
@@ -20,11 +13,6 @@ int main(int argc, char *argv[])
 	return (0);
 }
 
-/**
- * create_node - Creates a node.
- * @n: Number to go inside the node.
- * Return: Upon sucess a pointer to the node. Otherwise NULL.
- */
 stack_t *create_node(int n)
 {
 	stack_t *node;
@@ -38,9 +26,6 @@ stack_t *create_node(int n)
 	return (node);
 }
 
-/**
- * free_nodes - Frees nodes in the stack.
- */
 void free_nodes(void)
 {
 	stack_t *tmp;
@@ -56,12 +41,6 @@ void free_nodes(void)
 	}
 }
 
-
-/**
- * add_to_queue - Adds a node to the queue.
- * @new_node: Pointer to the new node.
- * @ln: line number of the opcode.
- */
 void add_to_queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
 {
 	stack_t *tmp;
